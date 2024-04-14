a6dafc375d7dd6623cea1c7462ff4e915e603345
Matheus <iam.mths.s@gmail.com>
2024-04-14 19:25:36 -0300

    módulo finalizado

--- Estudos/02 - Estruturas Condicionais e de Repetição em Python/02 - Estruturas Repetição - While.py
+++ Estudos/02 - Estruturas Condicionais e de Repetição em Python/02 - Estruturas Repetição - While.py
@@ -14,3 +14,13 @@ while opcao != 3:
     if opcao == 2:
         print(f'Seu saldo é de R$:\033[1;32m{saldo}\033[0m')
 print('\033[1;33mObrigado[a] por ser nosso cliente, Tenha um ótimo dia!\033[0m')
+
+
+while True: # condição infinita...
+    numero = int(input('Digite um numero: '))
+
+    if numero == 10:
+        break # força e parada a condição.
+
+    if numero % 2 == 0:
+        continue # Serve para pular todo o código a frente e voltar para o loop...
